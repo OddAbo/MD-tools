afc7b3f30b6909d660da56a9948175acc24b139a
OddAbo <398774325@qq.com>
2019-11-27 21:41:53 +0800

    demo

--- sisf.f90
+++ sisf.f90
@@ -10,14 +10,12 @@ program sisf
   real, parameter :: pi = 3.14159265, dt = 0.002
   real, parameter :: rmax = 10, dr = 0.1
   integer, parameter :: natom = 43, nstep = 1000000
-  integer :: iend, iatom1, iatom2, istep, ibox, nbox, i, j
-  real(kind=8) :: rv_0(6,natom), rv_new(6,natom), qmax, rdamp
-  real(kind=8) :: fs, dist, rmax2, gs(100)    ! gs(nbox)
+  integer :: iend, istep, i, j
+  real(kind=8) :: rv_0(6,natom), rv_new(6,natom), qmax
+  real(kind=8) :: fs, dist
   
-  nbox = int(rmax / dr)
-  rmax2 = rmax * rmax
   qmax = 2 * pi / 2.7
-  rdamp = rmax / sqrt(3.0)
+
   inquire(file="md.out",exist=iexist)
   if(.not.iexist) then
     stop "Error: ""md.out"" doesn't exist!"
@@ -31,37 +29,25 @@ program sisf
   ! endif
 
   open(10,file="md.out",form="unformatted",status="old")
-  open(20,file="vhcf.dat", form="formatted",status="unknown")
+  open(20,file="sisf.dat", form="formatted",status="unknown")
   rewind(10)
   rewind(20)
   write(20,*) "t      r      Gs(r,t)"
   
   read(10) istep, rv_0
-  rewind(10)
+  ! rewind(10)
 
   loop: do i = 1, nstep
-    fs = 0
     read(10,iostat=iend) istep, rv_new
     if(is_iostat_end(iend)) exit loop
-
-    ! Self part of van Hove correlation function
-    gs = 0
-    particle1: do iatom1 = 1, natom
-      particle2: do iatom2 = 1, natom
-        dist = sum((rv_new(1:3,iatom1) - rv_0(1:3,iatom1))**2)
-        ! if (dist > rmax2) cycle particle
-        dist = sqrt(dist)
-        ibox = int(dist/dr) + 1
-        gs(ibox) = gs(ibox) + 1
-      enddo particle2
-    enddo particle1
-
-    gs = gs / float(natom)
-
-    ! Fs from space Fourier transform of Gs
-    do j = 1, nbox
-      fs = fs + gs(j) * cos((j-1) * dr * qmax) * dr! * exp(-((j-1)*dr/rdamp)**2)
+    fs = 0
+    
+    do j = 1, natom
+      dist = sum((rv_new(1:3,j)-rv_0(1:3,j))**2)
+      dist = sqrt(dist)
+      fs = fs + cos(qmax*dist)
     enddo
+    fs = fs / float(natom)
     write(20,"(2f10.6)") (i-1)*dt, fs
     if (fs < 0) exit loop
   enddo loop
